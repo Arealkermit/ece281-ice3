b556426e81ad687189ff7417ea878be5a8a1376b
AFolsom <folsomam@outlook.com>
2026-02-11 13:46:35 -0700

    Cases for ripple adder

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,7 +49,26 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-	
+	   w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "0+0+Cin" severity failure;
+	   w_addends <= x"0F"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "F+0+Cin ripple" severity failure;
+	   w_addends <= x"1F"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "F+1+0" severity failure;
+	   w_addends <= x"88"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "8+8" severity failure;
+	   w_addends <= x"17"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"8" and w_Cout = '0') report "7+1" severity failure;
+	   w_addends <= x"A5"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"F" and w_Cout = '0') report "5+A" severity failure;
+	   w_addends <= x"3C"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "C+3+1" severity failure;
+	   
+	   
+	   
+	   
+	   
+	   
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
